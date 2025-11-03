42fba239be87e6c20734b2652a126f01786aacb6
Natalie Uranes <natalie.uranes@nav.no>
2025-11-03 16:02:24 +0100

    chore: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Preload from "@/app/preload";
 
 export const metadata: Metadata = {
   title: "TSM manuell",
@@ -13,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="no">
+    <Preload />
       <body>
         {children}
       </body>
