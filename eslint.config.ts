0ea2d7b9a3d46e990e5fe2aea72760b44ad64bef
Natalie Uranes <natalie.uranes@nav.no>
2025-11-12 12:05:34 +0100

    chore: add rules for eslint and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'eslint/config'
+// @ts-expect-error Type definitions missing
+import nextVitals from 'eslint-config-next/core-web-vitals'
+// @ts-expect-error Type definitions missing
+import nextTs from 'eslint-config-next/typescript'
+import importAlias from '@limegrass/eslint-plugin-import-alias'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import tsmEslintReact from "@navikt/tsm-eslint-react";
+
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    ...tsmEslintReact,
+    {
+        extends: [eslintPluginPrettierRecommended],
+        rules: { 'prettier/prettier': 'warn' },
+    },
+    {
+        files: ['src/**/*.ts', 'src/**/*.tsx'],
+        plugins: { 'import-alias': importAlias },
+        rules: { 'import-alias/import-alias': 'error'}
+    }
+]);
+
+export default eslintConfig;
