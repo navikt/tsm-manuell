89dbd1f60ce56b9fb801b8bb5d370e6b9d9490b5
Karl J. Overå <k@rl.run>
2025-12-04 09:04:43 +0100

    chore: bump other deps, fix build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     output: 'standalone',
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+    serverExternalPackages: [
+        '@navikt/next-logger',
+        'next-logger',
+        'pino',
+        'pino-socket',
+        '@whatwg-node',
+        'prom-client',
+    ],
 };
 
 export default nextConfig
